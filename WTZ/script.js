26c486a69d6d71dda9f2ba76b920229b66003dbd
Katarzyna Madej <katarzyna.madej@hotmail.com>
2020-07-09 15:36:33 +0200

    rwd and burger

--- WTZ/script.js
+++ WTZ/script.js
@@ -1,3 +1,8 @@
 $(".burger").on("click", function () {
   $(".fas, nav").toggleClass("off");
 });
+
+$(".nav__trigger").on("click", function (e) {
+  e.preventDefault();
+  $(this).parent().toggleClass("nav--active");
+});
