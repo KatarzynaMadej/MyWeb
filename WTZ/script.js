d082ea7aaf22a08058a07295786171d350adb227
Katarzyna Madej <katarzyna.madej@hotmail.com>
2020-07-14 11:59:17 +0200

    burger&footer on link&menu

--- WTZ/script.js
+++ WTZ/script.js
@@ -1,8 +1,3 @@
 $(".burger").on("click", function () {
   $(".fas, nav").toggleClass("off");
 });
-
-$(".nav__trigger").on("click", function (e) {
-  e.preventDefault();
-  $(this).parent().toggleClass("nav--active");
-});
