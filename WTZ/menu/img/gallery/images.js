97649af9f3f19528aaee16578ef8ee21b62ed37b
Katarzyna Madej <katarzyna.madej@hotmail.com>
2020-07-15 17:07:08 +0200

    gallery

---
+++ WTZ/menu/img/gallery/images.js
@@ -0,0 +1,7 @@
+export default [
+  "artistic1.jpg",
+  "artistic.jpg",
+  "artistic3.jpg",
+  "artistic2.jpg",
+  "artistic4.jpg",
+];
--- WTZ/script.js
+++ WTZ/script.js
@@ -1,3 +1,20 @@
 $(".burger").on("click", function () {
   $(".fas, nav").toggleClass("off");
 });
+
+function myFunction(imgs) {
+  const expandImg = document.getElementById("expandedImg");
+  const imgText = document.getElementById("imgtext");
+  const background = document.getElementById("row");
+  expandImg.src = imgs.src;
+  imgText.innerHTML = imgs.alt;
+  expandImg.parentElement.style.display = "block";
+  background.classList.toggle("blur");
+}
+
+function closeImg() {
+  const span = document.querySelector("span");
+  const background = document.getElementById("row");
+  span.parentElement.style.display = "none";
+  background.classList.toggle("blur");
+}
