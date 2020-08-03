73704aba07908bdf22abd759111b2945949ce616
Katarzyna Madej <katarzyna.madej@hotmail.com>
2020-08-03 15:20:25 +0200

    Updates

---
+++ precache-manifest.a5e968c582876bfda39b0f298051b8fa.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05c279d364cbd64274027066cb28be95",
+    "url": "/MyWeb/index.html"
+  },
+  {
+    "revision": "cefc7206f1aba2c87164",
+    "url": "/MyWeb/static/css/main.584a2639.chunk.css"
+  },
+  {
+    "revision": "e5e4e0825cb2f4ffac7c",
+    "url": "/MyWeb/static/js/2.3ddbd8be.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MyWeb/static/js/2.3ddbd8be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cefc7206f1aba2c87164",
+    "url": "/MyWeb/static/js/main.789d1c47.chunk.js"
+  },
+  {
+    "revision": "7850548ec1c2f6ab1edd",
+    "url": "/MyWeb/static/js/runtime-main.f66c16c2.js"
+  },
+  {
+    "revision": "35e3a67851b5e295c0301345cc93097c",
+    "url": "/MyWeb/static/media/book01.35e3a678.jpg"
+  },
+  {
+    "revision": "79a60b0eb038676e60ce019f14ba1268",
+    "url": "/MyWeb/static/media/book02.79a60b0e.jpg"
+  },
+  {
+    "revision": "4cde8224a45036c6d11b44128b4a7936",
+    "url": "/MyWeb/static/media/book03.4cde8224.jpg"
+  },
+  {
+    "revision": "56e574334ebac0062ce31038903a67b5",
+    "url": "/MyWeb/static/media/book04.56e57433.jpg"
+  },
+  {
+    "revision": "d070c9ece3dbf9132bd993bc147a3692",
+    "url": "/MyWeb/static/media/book05.d070c9ec.jpg"
+  }
+]);
